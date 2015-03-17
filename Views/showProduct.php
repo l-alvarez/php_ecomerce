e49077646a79b6e266bbba7ff49f2adfc8a0f252
47828846-R <marc.serret@estudiants.urv.cat>
2015-03-17 20:41:02 +0100

    JJJJJJJJJJJJJ

--- Views/showProduct.php
+++ Views/showProduct.php
@@ -1,29 +1,26 @@
 <?php
 include '../Controllers/ProducteController.php';
-
 echo LABEL_PRODUCTES;
-
 $prod = new ProducteController();
 $resultado = $prod->selectById($_GET['id']);
 
 function sql_dump_result($result) {
     $line = '';
-    $head = '';
+    $line1 = '';
 
     while ($temp = mysql_fetch_assoc($result)) {
-        if (empty($head)) {
-            $keys = array_keys($temp);
-            $head = '<tr id="ctabla"><th>'.LABEL_ESTOC.'</th><th>'.LABEL_NOM.'</th><th>'.LABEL_FOTO.'</th></tr>';
-        }
-        $line .= '<tr><td></td><td><a href="../Controllers/Command.php?controller=ProducteController&action=show&id='.$temp['id_producte'].'">'.$temp['nom'].'</a></td><td><img src=' . $temp['url_foto'] . ' WIDTH=100 HEIGHT=100></td></tr>';
+        $line .= '<tr><td>' . $temp['nom'] . '</a></td><td><img src=' . $temp['url_foto'] . ' WIDTH=100 HEIGHT=100></td></tr>';
+        $line1.='<tr><td>' . $temp['desc_llarga'] . '</td></tr>';
     }
-    return '<table id="tabla productos">' . $head . $line . '</table>';
+    return '<table id="tabla productos">' . $line . $line1 . '</table>';
 }
-
-if (!$resultado)
+if (!$resultado) {
     die("Error: no se pudo realizar la consulta");
-
+}
 echo sql_dump_result($resultado);
 
+//echo '<table id = "temporizador"><tr><td> '. include'../javascript/contador.php' . '</td><td></td></tr></table >';
 ?>
+
+
 
--- javascript/contador.php
+++ javascript/contador.php
@@ -1,39 +1,39 @@
 
 
 <div id="div_session_timeout" class="ui-state-highlight" style="float: left; width: 30%;  font-size: .8em; text-align: center;">
-                    <script language="javascript" type="text/javascript">
-                        /* Visit http://www.yaldex.com/ for full source code and get more free JavaScript, CSS and DHTML scripts! */
-                        var dateStamp = new Date("<?php echo date('D M j Y H:i:s'); ?>"); // Obtener la fecha y hora actual del servidor en este formato: Sun May 23 2010 20:14:11
-                        var intStamp = Number(dateStamp); // Convertir a timestamp la fecha y hora actual del servidor
-                        //Begin
-                        function getTime() {
-                            now = new Date(intStamp); // Trabajar con el formato timestamp
-                            // Obtener la fecha y hora en la que deberá terminar la sessión
-                            // en mi caso, la sesión la tengo establecida a 3600 segundos
-                            y2k = new Date("<?php echo date('M j Y H:i:s', time() + 3600); ?>");
-                            days = (y2k - now) / 1000 / 60 / 60 / 24;
-                            daysRound = Math.floor(days);
-                            hours = (y2k - now) / 1000 / 60 / 60 - (24 * daysRound);
-                            hoursRound = Math.floor(hours);
-                            minutes = (y2k - now) / 1000 / 60 - (24 * 60 * daysRound) - (60 * hoursRound);
-                            minutesRound = Math.floor(minutes);
-                            seconds = (y2k - now) / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) - (60 * minutesRound);
-                            secondsRound = Math.round(seconds);
-                            sec = (secondsRound == 1) ? " segundo" : " segundos";
-                            min = (minutesRound == 1) ? " minuto, " : " minutos, ";
-                            hr = (hoursRound == 1) ? " hora, " : " horas, ";
-                            dy = (daysRound == 1) ? " d\355a, " : " d\355as, "
-                            //document.timeForm.input1.value = "Time remaining: " + daysRound  + dy + hoursRound + hr + minutesRound + min + secondsRound + sec;
-                            if (daysRound + hoursRound + minutesRound + secondsRound == '0000') {
-                                document.getElementById("session_timeout").innerHTML = '<span style="color: red;">La sesi\363n ha expirado.</span><br />' + hoursRound + hr + minutesRound + min + secondsRound + sec;
-                            } else {
-                                document.getElementById("session_timeout").innerHTML = "Tiempo restante de esta sesi\363n: <br />" + hoursRound + hr + minutesRound + min + secondsRound + sec;
-                                newtime = window.setTimeout("getTime();", 1000);
-                                intStamp = intStamp + 1000; // Para avanzar un segundo en cada iteracion a partir de la fecha y hora actual obtenida desde el servidor
-                            } // endif
-                        } // end function
-                        window.onload = getTime;
-                        //  End -->
-                    </script>
-                    <span id="session_timeout"></span>
-                </div>
+    <script language="javascript" type="text/javascript">
+        /* Visit http://www.yaldex.com/ for full source code and get more free JavaScript, CSS and DHTML scripts! */
+        var dateStamp = new Date("<?php echo date('D M j Y H:i:s'); ?>"); // Obtener la fecha y hora actual del servidor en este formato: Sun May 23 2010 20:14:11
+        var intStamp = Number(dateStamp); // Convertir a timestamp la fecha y hora actual del servidor
+        //Begin
+        function getTime() {
+            now = new Date(intStamp); // Trabajar con el formato timestamp
+            // Obtener la fecha y hora en la que deberá terminar la sessión
+            // en mi caso, la sesión la tengo establecida a 3600 segundos
+            y2k = new Date("<?php echo date('M j Y H:i:s', time() + 3600); ?>");
+            days = (y2k - now) / 1000 / 60 / 60 / 24;
+            daysRound = Math.floor(days);
+            hours = (y2k - now) / 1000 / 60 / 60 - (24 * daysRound);
+            hoursRound = Math.floor(hours);
+            minutes = (y2k - now) / 1000 / 60 - (24 * 60 * daysRound) - (60 * hoursRound);
+            minutesRound = Math.floor(minutes);
+            seconds = (y2k - now) / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) - (60 * minutesRound);
+            secondsRound = Math.round(seconds);
+            sec = (secondsRound == 1) ? " segundo" : " segundos";
+            min = (minutesRound == 1) ? " minuto, " : " minutos, ";
+            hr = (hoursRound == 1) ? " hora, " : " horas, ";
+            dy = (daysRound == 1) ? " d\355a, " : " d\355as, "
+            //document.timeForm.input1.value = "Time remaining: " + daysRound  + dy + hoursRound + hr + minutesRound + min + secondsRound + sec;
+            if (daysRound + hoursRound + minutesRound + secondsRound == '0000') {
+                document.getElementById("session_timeout").innerHTML = '<span style="color: red;">La sesi\363n ha expirado.</span><br />' + hoursRound + hr + minutesRound + min + secondsRound + sec;
+            } else {
+                document.getElementById("session_timeout").innerHTML = "Tiempo restante de esta sesi\363n: <br />" + hoursRound + hr + minutesRound + min + secondsRound + sec;
+                newtime = window.setTimeout("getTime();", 1000);
+                intStamp = intStamp + 1000; // Para avanzar un segundo en cada iteracion a partir de la fecha y hora actual obtenida desde el servidor
+            } // endif
+        } // end function
+        window.onload = getTime;
+        //  End -->
+    </script>
+    <span id="session_timeout"></span>
+</div>
